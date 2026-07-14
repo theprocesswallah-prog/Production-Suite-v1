30280ac07d64bf65d5b37aaf5bf0e75b99607689
theprocesswallah-prog <theprocesswallah@gmail.com>
2026-07-14 10:34:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Relative paths load karne ke liye isse configure karna zaroori hai
+})
